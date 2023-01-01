c15386b44778ae59f9cf6686315bbb1aea26cdbd
Suhasks010 <119952447+Suhasks010@users.noreply.github.com>
2023-01-01 20:12:36 +0530

    Added title label, start and reset button. Placed using grid function.

--- main.py
+++ main.py
@@ -28,10 +28,20 @@ window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx=50, pady=100)
 
-canvas = Canvas(width=200, height=224)
+canvas = Canvas(width=200, height=224, highlightthickness=0, bg=YELLOW)
 img = PhotoImage(file="tomato.png")
 
 canvas.create_image(100, 112, image=img)
-canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"))
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
+
+title_label = Label(text="Timer", fg=GREEN, highlightthickness=0, bg=YELLOW)
+title_label.grid(column=1, row=0)
+
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=3, row=2)
+
 window.mainloop()
